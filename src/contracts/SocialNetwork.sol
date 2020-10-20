6ac9a5e4c28e32a9980f86bc116683470425950e
Abey jose sebastian <abeyjosesebastian64@gmail.com>
2020-10-20 09:35:30 +0530

    CREATE POSTS

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -2,10 +2,38 @@ pragma solidity ^0.5.0;
 
 contract SocialNetwork{
 
+    uint public postCount = 0;
     string public name;
+    mapping(uint=> Post) public posts;
+
+
+    struct Post {
+        uint id;
+        string content;
+        uint tipAmount;
+        address author;
+    }
+
+    event PostCreated(
+        uint id,
+        string content,
+        uint tipAmount,
+        address author
+    );
 
     constructor() public {
         name="A decentralized social Network";
-    }    
+    } 
+
+    function createPost(string memory _content) public {
+        // Require valid content
+        require(bytes(_content).length > 0);
+        // Increment the post count
+        postCount ++;
+        // Create the post
+        posts[postCount] = Post(postCount, _content, 0, msg.sender);
+        // Trigger event
+        emit PostCreated(postCount, _content, 0, msg.sender);
+    }   
 
 }
