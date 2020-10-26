74e6cf6789577194370d3c3fbf470dc27af54ae6
Abey jose sebastian <abeyjosesebastian64@gmail.com>
2020-10-26 15:42:42 +0530

    TIP POSTS

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -11,14 +11,21 @@ contract SocialNetwork{
         uint id;
         string content;
         uint tipAmount;
-        address author;
+        address payable author;
     }
 
     event PostCreated(
         uint id,
         string content,
         uint tipAmount,
-        address author
+        address payable author
+    );
+
+    event PostTipped(
+        uint id,
+        string content,
+        uint tipAmount,
+        address payable author
     );
 
     constructor() public {
@@ -35,5 +42,22 @@ contract SocialNetwork{
         // Trigger event
         emit PostCreated(postCount, _content, 0, msg.sender);
     }   
+
+   function tipPost(uint _id) public payable {
+        // Make sure the id is valid
+        require(_id > 0 && _id <= postCount);
+        // Fetch the post
+        Post memory _post = posts[_id];
+        // Fetch the author
+        address payable _author = _post.author;
+        // Pay the author by sending them Ether
+        address(_author).transfer(msg.value);
+        // Increment the tip amount
+        _post.tipAmount = _post.tipAmount + msg.value;
+        // Update the post
+        posts[_id] = _post;
+        // Trigger an event
+        emit PostTipped(postCount, _post.content, _post.tipAmount, _author);
+    }
 
 }
